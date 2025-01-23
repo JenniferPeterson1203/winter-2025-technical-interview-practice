4d1580caa73997c071af133d975d28628cf8f0e4
Jennifer <jenniferpeterson@pursuit.org>
2025-01-23 10:59:55 -0500

    Day 1 warmup complete

--- lesson01-tdd/warmup.js
+++ lesson01-tdd/warmup.js
@@ -1,4 +1,14 @@
 // 1. Given an integer, n,  between 3 and 100, return a line with n '*' characters
+const starChars = (n) => {
+  let result = "";
+
+  for (let i = 1; i <= n; i++) {
+    result += "*";
+  }
+  return result;
+};
+console.log(starChars(5));
+console.log(starChars(12));
 
 // 5 => '*****'
 // 12 => '************'
@@ -9,7 +19,14 @@
 
 // 5 => '*   *'
 // 12 => '*          *'
+const starsAndSpaces = (n, m) => {
+  let result = "";
 
+  for (let i = 1; i < n; i++) {
+    if (i !== 1 || n !== n) {
+    }
+  }
+};
 // 3. Given two integers, n and m, create n lines of m characters. The first and last line should be made of all '*' and all the middle lines should start and end with '*' and have all spaces in the middle
 
 // 5,5 => *****
@@ -17,5 +34,17 @@
 //        *   *
 //        *   *
 //        *****
+
+function createPattern(n, m) {
+  for (let i = 0; i < n; i++) {
+    if (i === 0 || i === n - 1) {
+      console.log("*".repeat(m));
+    } else {
+      console.log("*" + " ".repeat(m - 2) + "*");
+    }
+  }
+}
+
+createPattern(5, 5);
 
 // Console.log the result of each of the above functions to test them. Run your file with node warmup.js and check the terminal for the correct output.
