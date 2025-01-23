22dbe9da6c800f5d8538a723d3d05e1086a37395
Jennifer <jenniferpeterson@pursuit.org>
2025-01-23 10:59:31 -0500

    Day 1 problem complete

--- lesson01-tdd/problem.js
+++ lesson01-tdd/problem.js
@@ -11,7 +11,35 @@
  * @returns {boolean} - True if the string is a palindrome, false otherwise.
  */
 function isPalindrome(str) {
-  // your code here
+  // input is a string
+  // output is a boolean
+
+  // use regex to get rid of any non alphanumerical characters including empty spaces
+  // make each character in the string lowercase
+  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  // create a pointer for the very first character in the string (left)
+  let left = 0;
+  // create a pointer for the very last character in the string (right)
+  let right = str.length - 1;
+
+  // use a while loop to iterate while the left index is less than the right index
+  while (left < right) {
+    // if the characters at the left and right index are not the same this is not a palindrome
+    // return false and end the function
+    if (str[right] !== str[left]) {
+      return false;
+    }
+    // if they are the same
+    // increase the left index so that it will move to the next character
+    left++;
+    // decrease the right index so that it will move to the previous character
+    right--;
+  }
+  // if each character is checked and the loop did not end prematurely, then this is a palindrome
+  // return true
+  return true;
 }
 
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
 module.exports = isPalindrome;
