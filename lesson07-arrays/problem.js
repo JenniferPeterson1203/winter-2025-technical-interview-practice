39e095e4be70d3fc00ac439ddc538650006c96c1
Jennifer <jenniferpeterson@pursuit.org>
2025-02-04 11:01:16 -0500

    lesson 07 arrays problem complete

--- lesson07-arrays/problem.js
+++ lesson07-arrays/problem.js
@@ -5,4 +5,10 @@
 // // removeDuplicates([1, 2, 3, 4, 5]); // => [1, 2, 3, 4, 5]
 module.exports = function removeDuplicates(arr) {
   // your code here
+
+  return [...new Set(arr)];
 };
+
+// console.log(removeDuplicates([1, 2, 3, 3, 4, 5]));
+// console.log(removeDuplicates([1, 1, 1, 1, 1, 1]));
+// console.log(removeDuplicates([1, 2, 3, 4, 5]));
