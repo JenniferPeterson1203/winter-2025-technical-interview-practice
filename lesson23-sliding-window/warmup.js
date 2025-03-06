a9a8a5cd2d2189875103a877f7b60b13ca257e6e
Pursuit Admin <pursuitadmin@Pursuits-Laptop.local>
2025-03-06 11:11:23 -0500

    warmup complete

--- lesson23-sliding-window/warmup.js
+++ lesson23-sliding-window/warmup.js
@@ -1,3 +1,18 @@
 // Using array methods such as reduce(), map(), or filter(), solve these problems: 
 
-// 1. Given a sorted array, return the largest sum between two numbers. 
+// 1. Given an array that is not sorted, return the largest sum between two numbers without sorting the array. 
+const largestSum = (arr) => {
+
+    return arr.slice(-2).reduce((acc, curr, index)=> acc + curr, 0)
+
+    /**The .slice() method in JavaScript returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. In the provided code, arr.slice(-2) extracts the last two elements of the array arr. */
+ 
+}
+ 
+
+console.log(largestSum([1, 2, 3, 4, 5])); // 9
+// console.log(largestSum([1, 2, 3, 4, 5, 6])); // 11
+// console.log(largestSum([1, 2, 3, 4, 5, 6, 7])); // 13
+// console.log(largestSum([1, 2, 3, 4, 5, 6, 7, 8])); // 15
+// console.log(largestSum([1, 2, 3, 4, 5, 6, 7, 8, 9])); // 17
+// console.log(largestSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 19
